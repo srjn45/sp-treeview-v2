a074c8ee6864f4c2e4c697fec9d6f1fc057a8fb7
srjn45 <srajanpathak45@gmail.com>
2018-06-30 23:00:59 +0530

    modified node template

--- src/app/sp-treeview/model/sp-treeview-node-template.ts
+++ src/app/sp-treeview/model/sp-treeview-node-template.ts
@@ -1,16 +1,24 @@
 import { Node } from "./node";
 import { Config } from "./config";
+import { MatRadioChange, MatCheckboxChange } from "@angular/material";
+import { EventEmitter } from "@angular/core";
 
 export interface SpTreeviewNodeTemplate {
 
     node: Node;
     config: Config;
 
-    onCollapseExpand: () => void;
-    onCheckChange: () => void;
-    onRadioChange: () => void;
-    onDelete: () => void;
-    onAddChild: () => void;
-    onLoadChildren: () => void;
+    delete: EventEmitter<Node>;
+    addChild: EventEmitter<Node>;
+    loadChildren: EventEmitter<Node>;
+    radioSelect: EventEmitter<Node[]>;
+    checkboxSelect: EventEmitter<Node[]>;
+
+    onCollapseExpand: (node: Node) => void;
+    onCheckChange: (node: MatCheckboxChange) => void;
+    onRadioChange: (node: MatRadioChange) => void;
+    onDelete: (node: Node) => void;
+    onAddChild: (node: Node) => void;
+    onLoadChildren: (node: Node) => void;
 
 }

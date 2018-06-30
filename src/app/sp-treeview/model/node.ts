68b2975349ff90aae6d74a530a93298b4a877558
srjn45 <srajanpathak45@gmail.com>
2018-06-30 21:44:32 +0530

    nodify to set prototype get checked value change child state change self state corresponding to child verify state

--- src/app/sp-treeview/model/node.ts
+++ src/app/sp-treeview/model/node.ts
@@ -1,10 +1,23 @@
-import { NodeLevelConfig } from "./config";
+import { NodeLevelConfig, CHECKED_VALUE_PARENT, CHECKED_VALUE_LEAVES } from "./config";
 
 export const CHECKED = 1;
 export const UNCHECKED = 0;
 export const INDETERMINATE = -1;
 
 export class Node {
+
+    /**
+     * recursively sets prototype Node on the object and its children
+     * 
+     * @param node 
+     */
+    public static nodify(node: any): Node {
+        node = Object.setPrototypeOf(node, Node.prototype);
+        if (node.children != null) {
+            node.children.forEach(n => this.nodify(n));
+        }
+        return node;
+    }
 
     constructor(
         private _name: string,
@@ -72,6 +85,120 @@ export class Node {
             this._children = [];
         }
         this._children.push(child);
+    }
+
+
+    public verifyStateRecursive() {
+        if (this.children == null) {
+            return;
+        }
+
+        if (this._nodeState.checked === CHECKED) {
+            this.changeChildrenRecursive();
+            return;
+        }
+
+        this.children.filter(n => n.children != null).forEach(n => n.verifyStateRecursive());
+
+        this.checkImmediateChildren();
+    }
+
+    public changeChildrenRecursive() {
+        console.log(this.name + this.nodeState.checked);
+        if (this.children == null) {
+            return;
+        }
+        this.children.forEach(n => {
+            console.log(n);
+            n.nodeState.checked = this.nodeState.checked;
+            n.changeChildrenRecursive();
+        });
+    }
+
+    public checkImmediateChildren() {
+        if (this.children) {
+            this.children.forEach(c => c.checkImmediateChildren());
+
+            let checkedChildren: number = this.children.filter(n => n.nodeState.checked === CHECKED).length;
+
+            let indeterminateChildren: number = this.children.filter(n => n.nodeState.checked === INDETERMINATE).length;
+
+            if (indeterminateChildren > 0) {
+                // if indeterminate child the indeterminate
+                this.nodeState.checked = UNCHECKED;
+            } else {
+                // if no indeterminate child
+                if (checkedChildren === this.children.length) {
+                    // if all checked then checked
+                    this.nodeState.checked = CHECKED;
+                } else if (checkedChildren === 0) {
+                    // if all unchecked then unchecked
+                    this.nodeState.checked = UNCHECKED;
+                } else {
+                    // if not all checked then indeterminate
+                    this.nodeState.checked = INDETERMINATE;
+                }
+            }
+        }
+    }
+
+    private checkedLeaves(): Node[] {
+        if (this.nodeState.checked === UNCHECKED) {
+            return [];
+        }
+        if (this.children) {
+            let values = [];
+            this.children.forEach(n => {
+                n.checkedLeaves().forEach(v => values.push(v));
+            });
+            return values;
+        } else {
+            return [this];
+        }
+    }
+
+    private checkedAll(): Node[] {
+        if (this.nodeState.checked === CHECKED || this.nodeState.checked === INDETERMINATE) {
+            if (this.children) {
+                let values = [];
+                if (this.nodeState.checked === CHECKED) {
+                    values.push(this);
+                }
+                this.children.forEach(n => {
+                    n.checkedAll().forEach(v => values.push(v));
+                });
+                return values;
+            } else {
+                return [this];
+            }
+        } else {
+            return [];
+        }
+    }
+
+    private checkedHighest(): Node[] {
+        if (this.nodeState.checked === CHECKED) {
+            return [this]
+        }
+        if (this.children) {
+            let values = [];
+            this.children.forEach(n => {
+                n.checkedHighest().forEach(v => values.push(v));
+            });
+            return values;
+        }
+        return [];
+    }
+
+    public getCheckedValues(checkedValue: number): Node[] {
+        if (checkedValue === CHECKED_VALUE_PARENT) {
+            return this.checkedHighest();
+        } else if (checkedValue === CHECKED_VALUE_LEAVES) {
+            return this.checkedLeaves();
+        } else {
+            // selected values all
+            return this.checkedAll();
+        }
     }
 
 }

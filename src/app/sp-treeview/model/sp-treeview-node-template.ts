84a17ec5b28b1f7f9eb1c3b956ca3fd0cdd6e108
srjn45 <srajanpathak45@gmail.com>
2018-06-30 21:46:08 +0530

    refactored  template

--- src/app/sp-treeview/model/sp-treeview-node-template.ts
+++ src/app/sp-treeview/model/sp-treeview-node-template.ts
@@ -11,6 +11,6 @@ export interface SpTreeviewNodeTemplate {
     onRadioChange: () => void;
     onDelete: () => void;
     onAddChild: () => void;
-    onLoadChild: () => void;
+    onLoadChildren: () => void;
 
 }

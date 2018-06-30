361bbb315f227c986a3d6313503e025c1db34532
srjn45 <srajanpathak45@gmail.com>
2018-06-30 16:01:06 +0530

    added treeview module and node component

---
+++ src/app/sp-treeview/sp-treeview-node/sp-treeview-node.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-treeview-node',
+  templateUrl: './sp-treeview-node.component.html',
+  styleUrls: ['./sp-treeview-node.component.css']
+})
+export class SpTreeviewNodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/sp-treeview/sp-treeview.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SpTreeviewNodeComponent } from './sp-treeview-node/sp-treeview-node.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SpTreeviewNodeComponent]
+})
+export class SpTreeviewModule { }

c371fdab6e1fecf11a68bff4e555c0f012fe3ac5
srjn45 <srajanpathak45@gmail.com>
2018-06-30 21:54:13 +0530

    handled varion events on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,19 @@ export class AppComponent {
   config = new Config(new TreeLevelConfig(true, SELECT_CHECKBOX, CHECKED_VALUE_LEAVES, true, true, true));
   node = new Node("name", 1, [new Node("child", 1, null, false, new NodeState(), new NodeLevelConfig())], false, new NodeState(), new NodeLevelConfig());
 
-  onLoadChild(node: Node) {
+  onLoadChildren(node: Node) {
     setTimeout(() => {
       node.children = [new Node("child1", 1, null, false, new NodeState(), new NodeLevelConfig()),
       new Node("child2", 2, null, false, new NodeState(), new NodeLevelConfig())];
     }, 3000);
   }
+
+  onDelete(node: Node) {
+    console.log("Delete" + JSON.stringify(node));
+  }
+
+  onAddChild(node: Node) {
+    console.log("AddChild" + JSON.stringify(node));
+  }
+
 }

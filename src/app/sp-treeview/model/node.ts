819bb7536759063752e131874b7f983f575f689a
srjn45 <srajanpathak45@gmail.com>
2018-06-30 20:13:15 +0530

    type checked all getter/setter added method addChild

--- src/app/sp-treeview/model/node.ts
+++ src/app/sp-treeview/model/node.ts
@@ -17,54 +17,61 @@ export class Node {
 
     }
 
-    get name() {
+    get name(): string {
         return this._name;
     }
 
-    set name(name) {
+    set name(name: string) {
         this._name = name;
     }
 
-    get value() {
+    get value(): any {
         return this._value;
     }
 
-    set value(value) {
+    set value(value: any) {
         this._value = value;
     }
 
-    get children() {
+    get children(): Node[] {
         return this._children;
     }
 
-    set children(children) {
+    set children(children: Node[]) {
         this._children = children;
         this._progress = false;
         this._nodeState.collapsed = false;
     }
 
-    get progress() {
+    get progress(): boolean {
         return this._progress;
     }
 
-    set progress(progress) {
+    set progress(progress: boolean) {
         this._progress = progress;
     }
 
-    get nodeState() {
+    get nodeState(): NodeState {
         return this._nodeState;
     }
 
-    set nodeState(nodeState) {
+    set nodeState(nodeState: NodeState) {
         this._nodeState = nodeState;
     }
 
-    get nodeLevelConfig() {
+    get nodeLevelConfig(): NodeLevelConfig {
         return this._nodeLevelConfig;
     }
 
-    set nodeLevelConfig(nodeLevelConfig) {
+    set nodeLevelConfig(nodeLevelConfig: NodeLevelConfig) {
         this._nodeLevelConfig = nodeLevelConfig;
+    }
+
+    public addChild(child: Node) {
+        if (this._children === null || this._children === undefined) {
+            this._children = [];
+        }
+        this._children.push(child);
     }
 
 }
@@ -79,27 +86,27 @@ export class NodeState {
         private _disabled = false
     ) { }
 
-    get checked() {
+    get checked(): number {
         return this._checked;
     }
 
-    set checked(checked) {
+    set checked(checked: number) {
         this._checked = checked;
     }
 
-    get collapsed() {
+    get collapsed(): boolean {
         return this._collapsed;
     }
 
-    set collapsed(collapsed) {
+    set collapsed(collapsed: boolean) {
         this._collapsed = collapsed;
     }
 
-    get disabled() {
+    get disabled(): boolean {
         return this._disabled;
     }
 
-    set disabled(disabled) {
+    set disabled(disabled: boolean) {
         this._disabled = disabled;
     }
 }

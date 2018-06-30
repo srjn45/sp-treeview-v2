106c6e697a474d2536066cb61adf39bb3a153676
srjn45 <srajanpathak45@gmail.com>
2018-06-30 19:37:37 +0530

    changed the members to private and added getter & setter

--- src/app/sp-treeview/model/node.ts
+++ src/app/sp-treeview/model/node.ts
@@ -7,14 +7,64 @@ export const INDETERMINATE = -1;
 export class Node {
 
     constructor(
-        public name: string,
-        public value: any,
-        public children?: Node[],
-        public progress = false,
-        public nodeState = new NodeState(),
-        public nodeLevelConfig = new NodeLevelConfig()
+        private _name: string,
+        private _value: any,
+        private _children?: Node[],
+        private _progress = false,
+        private _nodeState = new NodeState(),
+        private _nodeLevelConfig = new NodeLevelConfig()
     ) {
 
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(value) {
+        this._value = value;
+    }
+
+    get children() {
+        return this._children;
+    }
+
+    set children(children) {
+        this._children = children;
+        this._progress = false;
+        this._nodeState.collapsed = false;
+    }
+
+    get progress() {
+        return this._progress;
+    }
+
+    set progress(progress) {
+        this._progress = progress;
+    }
+
+    get nodeState() {
+        return this._nodeState;
+    }
+
+    set nodeState(nodeState) {
+        this._nodeState = nodeState;
+    }
+
+    get nodeLevelConfig() {
+        return this._nodeLevelConfig;
+    }
+
+    set nodeLevelConfig(nodeLevelConfig) {
+        this._nodeLevelConfig = nodeLevelConfig;
     }
 
 }
@@ -24,8 +74,32 @@ export class Node {
  */
 export class NodeState {
     constructor(
-        public checked = UNCHECKED,
-        public collapsed = false,
-        public disabled = false
+        private _checked = UNCHECKED,
+        private _collapsed = false,
+        private _disabled = false
     ) { }
+
+    get checked() {
+        return this._checked;
+    }
+
+    set checked(checked) {
+        this._checked = checked;
+    }
+
+    get collapsed() {
+        return this._collapsed;
+    }
+
+    set collapsed(collapsed) {
+        this._collapsed = collapsed;
+    }
+
+    get disabled() {
+        return this._disabled;
+    }
+
+    set disabled(disabled) {
+        this._disabled = disabled;
+    }
 }

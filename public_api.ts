95bb76c45c17b5a142a8bb2f291316b91ee69c11
srjn45 <srajanpathak45@gmail.com>
2018-07-01 23:39:00 +0530

    replaced previous packaging with packgr

---
+++ public_api.ts
@@ -0,0 +1,16 @@
+export * from './src/app/sp-treeview/sp-treeview.module';
+
+export * from './src/app/sp-treeview/model/node-state';
+export * from './src/app/sp-treeview/model/node';
+export * from './src/app/sp-treeview/model/dropdown-level-config';
+export * from './src/app/sp-treeview/model/tree-level-config';
+export * from './src/app/sp-treeview/model/node-level-config';
+export * from './src/app/sp-treeview/model/config';
+
+export * from './src/app/sp-treeview/model/sp-treeview-node-template';
+export * from './src/app/sp-treeview/model/sp-treeview-node-template-context';
+
+export * from './src/app/sp-treeview/sp-treeview-node/sp-treeview-node.component';
+export * from './src/app/sp-treeview/sp-treeview/sp-treeview.component';
+export * from './src/app/sp-treeview/sp-treeview-dropdown/sp-treeview-dropdown.component';
+export * from './src/app/sp-treeview/sp-treeview-overlay/sp-treeview-overlay.component';

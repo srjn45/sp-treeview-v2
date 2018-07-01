bec06f73437fcf35255a61cba61fe56505ac5ff3
srjn45 <srajanpathak45@gmail.com>
2018-07-01 14:12:10 +0530

    input overlay component

--- src/app/sp-treeview/sp-treeview.module.ts
+++ src/app/sp-treeview/sp-treeview.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpTreeviewNodeComponent } from './sp-treeview-node/sp-treeview-node.component';
-import { MatProgressBarModule, MatCheckboxModule, MatRadioModule, MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+import { MatProgressBarModule, MatCheckboxModule, MatRadioModule, MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatChipsModule } from '@angular/material';
 import { SpTreeviewComponent } from './sp-treeview/sp-treeview.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SpTreeviewOverlayComponent } from './sp-treeview-overlay/sp-treeview-overlay.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserAnimationsModule,
     MatProgressBarModule,
     MatCheckboxModule,
     MatRadioModule,
@@ -15,11 +17,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
-    BrowserAnimationsModule
+    MatChipsModule
   ],
-  declarations: [SpTreeviewNodeComponent, SpTreeviewComponent],
+  declarations: [SpTreeviewNodeComponent, SpTreeviewComponent, SpTreeviewOverlayComponent],
   exports: [
-    SpTreeviewComponent
+    SpTreeviewComponent,
+    SpTreeviewOverlayComponent
   ]
 })
 export class SpTreeviewModule { }
